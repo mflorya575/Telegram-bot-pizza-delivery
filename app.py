e7fb8cdfd625d460b6c0f56a9054e1778f640c43
Michael Florya <mih2303@mail.ru>
2024-02-22 19:56:10 +0300

    переписана клавиатура, добавлена команда админа и его клавиши

--- app.py
+++ app.py
@@ -9,18 +9,25 @@ load_dotenv(find_dotenv())
 
 from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
+from handlers.admin_private import admin_router
+
 from common.bot_cmds_list import private
 
+
 ALLOWED_UPDATES = ['message, edited_message']
+
 bot = Bot(token=os.getenv('TOKEN'), parse_mode=ParseMode.HTML)
+bot.my_admins_list = []
+
 dp = Dispatcher()
 
 dp.include_router(user_private_router)
 dp.include_router(user_group_router)
+dp.include_router(admin_router)
 
 
 async def main():
-    await bot.delete_webhook()
+    await bot.delete_webhook(drop_pending_updates=True)
     await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,24 +1,40 @@
 from aiogram import F, types, Router
+from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command, or_f
 from filters.chat_types import ChatTypeFilter
-from aiogram.utils.formatting import as_list, as_marked_section, Bold
+from aiogram.utils.formatting import (
+    as_list,
+    as_marked_section,
+    Bold,
+)
 
 from filters.chat_types import ChatTypeFilter
 
-from kbds import reply
+from kbds.reply import get_keyboard
+
 
 user_private_router = Router()
-# user_private_router.message.filter(ChatTypeFilter(['private']))
+user_private_router.message.filter(ChatTypeFilter(["private"]))
 
 
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer('Привет! Я виртуальный помощник', reply_markup=reply.start_kb)
+    await message.answer(
+        "Привет, я виртуальный помощник",
+        reply_markup=get_keyboard(
+            "Меню",
+            "О магазине",
+            "Варианты оплаты",
+            "Варианты доставки",
+            placeholder="Что вас интересует?",
+            sizes=(2, 2)
+        ),
+    )
 
 
 @user_private_router.message(or_f(Command('menu'), (F.text.lower() == 'меню')))
 async def menu_cmd(message: types.Message):
-    await message.answer('Вот меню:', reply_markup=reply.del_kbd)
+    await message.answer('Вот меню:')
 
 
 @user_private_router.message(F.text.lower() == 'о магазине')
@@ -61,6 +77,18 @@ async def delivery_cmd(message: types.Message):
         sep='\n----------------------\n'
     )
     await message.answer(text.as_html())
+
+
+# @user_private_router.message(F.contact)
+# async def get_contact(message: types.Message):
+#     await message.answer(f"номер получен")
+#     await message.answer(str(message.contact))
+
+
+# @user_private_router.message(F.location)
+# async def get_location(message: types.Message):
+#     await message.answer(f"локация получена")
+#     await message.answer(str(message.location))
 
 
 
